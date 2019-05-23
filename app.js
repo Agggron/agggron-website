0fb086f709625420d0c53f4d13da3049e60743da
Aaron Tang <aaronwt@berkeley.edu>
2019-05-23 00:47:40 -0500

    implemented JS scrolling to anchors upon click

--- app.js
+++ app.js
@@ -7,9 +7,19 @@ const fade_in_elements = function() {
 		if (window_bottom > (object_bottom + (object_height / 4))) {
 			$(this).animate({
 				opacity: 1
-			}, 1000);
+			}, 600);
 		}
 	});
+}
+
+const scroll_to_anchor = function(event) {
+	var anchor_id = event.data.param1
+	console.log("scroll_to_anchor(): anchor_id %s", anchor_id);
+	var anchor = $('#anchor_' + anchor_id);
+	var header_offset = (9 * $('header').outerHeight()) / 10;
+	$('html, body').animate({
+		scrollTop: Math.max(anchor.offset().top - header_offset, 0)
+	}, 1000);
 }
 
 $(document).ready(function() {
@@ -25,10 +35,17 @@ $(document).ready(function() {
 		console.log("toggle");
 	});
 
-	var navigation_hidden_links = ['#header_navigation_hidden_about', '#header_navigation_hidden_projects', '#header_navigation_hidden_resume', '#header_navigation_hidden_contact']
-	for (i = 0; i < navigation_hidden_links.length; i++) {
-		$(navigation_hidden_links[i]).click(function() {
-			$('.header_container_hidden').slideToggle();
-		})
+	// var navigation_hidden_links = ['#header_navigation_hidden_about', '#header_navigation_hidden_projects', '#header_navigation_hidden_resume', '#header_navigation_hidden_contact']
+	// for (i = 0; i < navigation_hidden_links.length; i++) {
+	// 	$(navigation_hidden_links[i]).click(function() {
+	// 		$('.header_container_hidden').slideToggle();
+	// 	})
+	// }
+
+	// Set targets for anchor links => navigation menu items will scroll to the appropriate anchor point.
+	var anchors = ['top', 'about', 'projects', 'resume', 'contact'];
+	for (i = 0; i < anchors.length; i++) {
+		var anchor = anchors[i];
+		$('.scroll_to_' + anchor).click({param1: anchor}, scroll_to_anchor);
 	}
 });
